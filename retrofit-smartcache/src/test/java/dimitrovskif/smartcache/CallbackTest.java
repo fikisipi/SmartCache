b28695a066c8121eddf1de4d4fdf1029ca7b550e
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2021-04-26 13:49:59 +0200

    Refactor tests into multiple subroutines

--- retrofit-smartcache/src/test/java/dimitrovskif/smartcache/CallbackTest.java
+++ retrofit-smartcache/src/test/java/dimitrovskif/smartcache/CallbackTest.java
@@ -1,5 +1,7 @@
 package dimitrovskif.smartcache;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -33,25 +35,38 @@ public class CallbackTest {
         return factory;
     }
 
+    private DemoService demoService = null;
+    private Logger log = null;
+
+    @Before
+    public void setUp() {
+        Retrofit r = new Retrofit.Builder()
+                .baseUrl(server.url("/"))
+                .addConverterFactory(new ToStringConverterFactory())
+                .addCallAdapterFactory(buildSmartCacheFactory())
+                .build();
+        this.demoService = r.create(DemoService.class);
+        log = Logger.getLogger("test_logger");
+    }
+
+    @After
+    public void tearDown() {
+        this.demoService = null;
+    }
+
     @Rule public final MockWebServer server = new MockWebServer();
+
     @Test
-    public void dispatch_isGood() throws Exception{
+    public void simpleNetworkTest() throws Exception {
         /* Set up the mock webserver */
         MockResponse resp = new MockResponse();
         resp.setBody("VERY_BASIC_BODY");
         server.enqueue(resp);
         server.enqueue(resp.clone());
 
-        Retrofit r = new Retrofit.Builder()
-                .baseUrl(server.url("/"))
-                .addConverterFactory(new ToStringConverterFactory())
-                .addCallAdapterFactory(buildSmartCacheFactory())
-                .build();
-        DemoService demoService = r.create(DemoService.class);
-
 
         final Logger log = Logger.getLogger("test_logger");
-        final CountDownLatch latch = new CountDownLatch(1);
+        final CountDownLatch latch = new CountDownLatch(2);
         final AtomicReference<Response<String>> responseRef = new AtomicReference<>();
         demoService.getHome().enqueue(new Callback<String>() {
             @Override
@@ -66,20 +81,54 @@ public class CallbackTest {
                 fail("Failure executing the request.");
             }
         });
-        assertTrue(latch.await(1, TimeUnit.SECONDS));
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals(latch.getCount(), 1);
         assertEquals(responseRef.get().body(), "VERY_BASIC_BODY");
-        final CountDownLatch latch2 = new CountDownLatch(2);
-        final AtomicReference<Response<String>> response2Ref = new AtomicReference<>();
+        assertTrue("No network flag on response.", SmartCache.isResponseFromNetwork(responseRef.get()));
+    }
+
+    @Test
+    public void twoTestCheck() throws Exception{
+        /* Set up the mock webserver */
+        MockResponse resp = new MockResponse();
+        resp.setBody("VERY_BASIC_BODY");
+        server.enqueue(resp);
+        server.enqueue(resp.clone());
+
+        final CountDownLatch latch = new CountDownLatch(2);
+        final AtomicReference<Response<String>> responseRef = new AtomicReference<>();
         demoService.getHome().enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                latch2.countDown();
-                if(latch2.getCount() == 1){ // the cache hit one.
-                    log.info("Got a response. Should be a cache hit.");
-                    response2Ref.set(response);
-                }else{ // the network one.
-                    log.info("Got a response. Should be a real network hit.");
-                    assertEquals(response.body(), response2Ref.get().body());
+                log.info("Got a response in pass #1 in twoTestCheck() call.");
+                responseRef.set(response);
+                latch.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                fail("Failure executing the request.");
+            }
+        });
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals(latch.getCount(), 1);
+        assertEquals(responseRef.get().body(), "VERY_BASIC_BODY");
+        assertTrue("Response isn't from network.", SmartCache.isResponseFromNetwork(responseRef.get()));
+
+        final CountDownLatch doubleLatch = new CountDownLatch(2);
+        final AtomicReference<Response<String>> secondTestResponse = new AtomicReference<>();
+        final AtomicReference<Response<String>> secondTestResponse2 = new AtomicReference<>();
+
+        demoService.getHome().enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                doubleLatch.countDown();
+                if(doubleLatch.getCount() == 1){
+                    secondTestResponse.set(response);
+                    log.info("Got a response in pass #2 in twoTestCheck() call. [latch = 1]");
+                } else { // the network one.
+                    secondTestResponse2.set(response);
+                    log.info("Got a response in pass #2 in twoTestCheck() call. [latch = 2]");
                 }
             }
 
@@ -88,32 +137,43 @@ public class CallbackTest {
                 fail("Failure executing the request.");
             }
         });
-        assertTrue(latch2.await(1, TimeUnit.SECONDS));
 
+        assertTrue(doubleLatch.await(1, TimeUnit.SECONDS));
+        Response<String> resp1 = secondTestResponse.get();
+        Response<String> resp2 = secondTestResponse2.get();
+
+        assertNotNull(resp1);
+        assertNotNull(resp2);
+
+        assertFalse(SmartCache.isResponseFromNetwork(secondTestResponse.get()));
+        assertTrue(SmartCache.isResponseFromNetwork(secondTestResponse2.get()));
+    }
+
+    @Test
+    public void testDisabledCache() throws Exception {
+        MockResponse resp = new MockResponse();
+        resp.setBody("VERY_BASIC_BODY");
+        server.enqueue(resp);
         server.enqueue(resp.clone());
 
-        final CountDownLatch latch3 = new CountDownLatch(2);
-        final AtomicInteger postResponses = new AtomicInteger(0);
-        // final AtomicReference<Response<String>> response3Ref = new AtomicReference<>();
-        demoService.postHome().enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                log.info("Got post response");
-                postResponses.incrementAndGet();
-                latch3.countDown();
-            }
+        for(int i = 0; i < 2; i++) {
+            log.info("Doing disabled test cache twice. i=" + String.valueOf(i));
+            CountDownLatch latch = new CountDownLatch(2);
+            demoService.postHome().enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    latch.countDown();
+                    log.info("Got a response in testDisabledCache()");
+                }
 
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                fail("Failure executing the request.");
-            }
-        });
-        latch3.await(1, TimeUnit.SECONDS);
-        assertEquals(1, postResponses.get());
-
-        server.enqueue(resp.clone());
-
-        assertTrue("Synchronous call works.", demoService.getHome().execute().body().length() > 0);
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    log.warning("Got a failure in testDisabledCache()");
+                }
+            });
+            latch.await(1, TimeUnit.SECONDS);
+            assertEquals(latch.getCount(), 1);
+        }
     }
 
     static class MainThreadExecutor implements Executor{
